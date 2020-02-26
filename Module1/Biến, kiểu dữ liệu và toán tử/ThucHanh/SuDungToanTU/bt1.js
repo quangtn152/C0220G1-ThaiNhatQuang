c63f73bbb06e6b9b243e908896f4bca3121be52c
wangtnq <wangtnq@gmail.com>
2020-02-26 09:46:45 +0700

    Su dung toan tu

---
+++ Module1/Biến, kiểu dữ liệu và toán tử/ThucHanh/SuDungToanTU/bt1.js
@@ -0,0 +1,8 @@
+let inputHeight;
+let inputWidth;
+inputWidth= prompt('Nhap chieu rong');
+inputHeight= prompt('Nhap chieu cao');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("area: "+area)
